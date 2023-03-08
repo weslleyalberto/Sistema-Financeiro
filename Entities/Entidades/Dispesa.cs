f0f4ff36b88dc1135e59f10127bdf7032df5b0f6
Weslley Alberto <weslleyalberto@gmail.com>
2023-03-08 14:46:14 -0300

    Criada Entidades do projeto

---
+++ Entities/Entidades/Dispesa.cs
@@ -0,0 +1,25 @@
+﻿using Entities.Enums;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Entidades
+{
+    [Table("Dispesa")]
+    public class Dispesa : Base
+    {
+        public decimal Valor { get; set; }
+        public int Mes { get; set; }
+        public int Ano { get; set; }
+        public EnumTipoDispesa TipoDispesa { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public DateTime DataAlteracao { get; set; }
+        public DateTime DataPagamento { get; set; }
+        public DateTime DataVencimento { get; set; }
+        public bool Pago { get; set; }
+        public bool DispesaAtrasada { get; set; }
+
+        [ForeignKey("Categoria")]
+        [Column(Order =1)]
+        public int IdCategoria { get; set; }
+        public virtual Categoria Categoria { get; set; }
+    }
+}
---
+++ Entities/Enums/EnumTipoDispesa.cs
@@ -0,0 +1,8 @@
+﻿namespace Entities.Enums
+{
+    public enum EnumTipoDispesa
+    {
+        Contas =1,
+        Investimento =2
+    }
+}
